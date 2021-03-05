7973110736d35d8b5f6ce1c99b41b0f6a25be794
Samuel Renken <sam@apruve.com>
2021-03-05 11:53:42 -0600

    Remove CORS #fetchProxy

--- htmlpreview.js
+++ htmlpreview.js
@@ -9,30 +9,12 @@
 		//Framesets
 		if (document.querySelectorAll('frameset').length)
 			return; //Don't replace CSS/JS if it's a frameset, because it will be erased by document.write()
-		//Frames
-		frame = document.querySelectorAll('iframe[src],frame[src]');
-		for (i = 0; i < frame.length; ++i) {
-			src = frame[i].src; //Get absolute URL
-			if (src.indexOf('//raw.githubusercontent.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-				frame[i].src = '//' + location.hostname + location.pathname + '?' + src; //Then rewrite URL so it can be loaded using CORS proxy
-			}
-		}
-		//Links
-		a = document.querySelectorAll('a[href]');
-		for (i = 0; i < a.length; ++i) {
-			href = a[i].href; //Get absolute URL
-			if (href.indexOf('#') > 0) { //Check if it's an anchor
-				a[i].href = '//' + location.hostname + location.pathname + location.search + '#' + a[i].hash.substring(1); //Then rewrite URL with support for empty anchor
-			} else if ((href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0) && (href.indexOf('.html') > 0 || href.indexOf('.htm') > 0)) { //Check if it's from raw.github.com or bitbucket.org and to HTML files
-				a[i].href = '//' + location.hostname + location.pathname + '?' + href; //Then rewrite URL so it can be loaded using CORS proxy
-			}
-		}
 		//Stylesheets
 		link = document.querySelectorAll('link[rel=stylesheet]');
 		for (i = 0; i < link.length; ++i) {
 			href = link[i].href; //Get absolute URL
 			if (href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-				links.push(fetchProxy(href, null, 0)); //Then add it to links queue and fetch using CORS proxy
+				links.push(fetch(href, null)); //Then add it to links queue and fetch
 			}
 		}
 		Promise.all(links).then(function (res) {
@@ -45,7 +27,7 @@
 		for (i = 0; i < script.length; ++i) {
 			src = script[i].src; //Get absolute URL
 			if (src.indexOf('//raw.githubusercontent.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-				scripts.push(fetchProxy(src, null, 0)); //Then add it to scripts queue and fetch using CORS proxy
+				scripts.push(fetch(src, null)); //Then add it to scripts queue and fetch
 			} else {
 				script[i].removeAttribute('type');
 				scripts.push(script[i].innerHTML); //Add inline script to queue to eval in order
@@ -85,22 +67,6 @@
 			script.innerHTML = data;
 			document.body.appendChild(script);
 		}
-	};
-	
-	var fetchProxy = function (url, options, i) {
-		var proxy = [
-			'https://cors-anywhere.herokuapp.com/',
-			'https://yacdn.org/proxy/',
-			'https://api.codetabs.com/v1/proxy/?quest='
-		];
-		return fetch(proxy[i] + url, options).then(function (res) {
-			if (!res.ok) throw new Error('Cannot load ' + url + ': ' + res.status + ' ' + res.statusText);
-			return res.text();
-		}).catch(function (error) {
-			if (i === proxy.length - 1)
-				throw error;
-			return fetchProxy(url, options, i + 1);
-		})
 	};
 
 	if (url && url.indexOf(location.hostname) < 0)
